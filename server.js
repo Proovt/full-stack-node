55bbe9a7061ec5ae7a26283786700ca744e2f1ef
Proovt <47065617+Proovt@users.noreply.github.com>
2023-09-08 12:39:25 +0200

    got rid of unused url parser option

--- server.js
+++ server.js
@@ -34,9 +34,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-})
+mongoose.connect(process.env.DATABASE_URL)
+    .catch(error => console.log(error))
 
 const db = mongoose.connection
 db.on('error', err => console.log(err))
